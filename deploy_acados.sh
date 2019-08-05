8cad701d145e67b18c3eacb5ae9317718ffacb3c
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-08-05 14:47:16 +0200

    unify deploy win linux, checkout submodules

---
+++ deploy_acados.sh
@@ -0,0 +1,57 @@
+set -e
+
+ACADOS_COMMIT_ID=41d8e7df52f43ed20057a16df4c846be186c5fc6
+ACADOS_SHORT_ID=41d8e7
+
+git clone git@github.com:acados/acados.git
+cd acados
+git checkout 41d8e7df52f43ed20057a16df4c846be186c5fc6
+git submodule init
+git submodule update
+cd ..
+
+# linux
+ACADOS_VERSION=${ACADOS_SHORT_ID}_linux
+OUTPUT_DIR=output_tmp_$ACADOS_VERSION
+
+cp acados $OUTPUT_DIR
+
+mkdir $OUTPUT_DIR/lib
+
+cp README.txt $OUTPUT_DIR
+cp lib_linux/libacore.so $OUTPUT_DIR/lib
+cp lib_linux/libacados.so $OUTPUT_DIR/lib
+cp lib_linux/libblasfeo.so $OUTPUT_DIR/lib
+cp lib_linux/libhpipm.so $OUTPUT_DIR/lib
+
+cd $OUTPUT_DIR
+zip -r -q acados-${ACADOS_VERSION}.zip .
+cd ..
+mv $OUTPUT_DIR/acados-${ACADOS_VERSION}.zip output
+
+rm -rf $OUTPUT_DIR
+rm acados_src.zip
+
+# windows
+ACADOS_VERSION=${ACADOS_SHORT_ID}_win
+OUTPUT_DIR=output_tmp_$ACADOS_VERSION
+
+cp acados $OUTPUT_DIR
+
+mkdir $OUTPUT_DIR/lib
+
+cp README.txt $OUTPUT_DIR
+cp lib_win/libacados.lib $OUTPUT_DIR/lib
+cp lib_win/libblasfeo.lib $OUTPUT_DIR/lib
+cp lib_win/libhpipm.lib $OUTPUT_DIR/lib
+
+cd $OUTPUT_DIR
+zip -r -q acados-${ACADOS_VERSION}.zip .
+cd ..
+mv $OUTPUT_DIR/acados-${ACADOS_VERSION}.zip output
+
+rm -rf $OUTPUT_DIR
+rm acados_src.zip
+
+# cleanup
+rm -rf acados
