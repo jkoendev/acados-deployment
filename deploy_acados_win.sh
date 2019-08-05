219d6078ca1a1edeac3e598c1c05d13b82cedea1
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-08-05 11:37:32 +0200

    deploy output dir, ignore tmp

--- deploy_acados_win.sh
+++ deploy_acados_win.sh
@@ -2,8 +2,6 @@ ACADOS_COMMIT_ID=41d8e7df52f43ed20057a16df4c846be186c5fc6
 ACADOS_VERSION=41d8e7_win
 OUTPUT_DIR=output_tmp_$ACADOS_VERSION
 
-
-
 DOWNLOAD_URL=https://github.com/acados/acados/archive/$ACADOS_COMMIT_ID.zip
 wget $DOWNLOAD_URL -O acados_src.zip
 
@@ -19,6 +17,3 @@ cp lib/libhpipm.lib $OUTPUT_DIR/lib
 cd $OUTPUT_DIR
 zip -r ../acados-${ACADOS_VERSION}.zip .
 cd ..
-mv $OUTPUT_DIR/acados-${ACADOS_VERSION}.zip .
-
-rm -r $OUTPUT_DIR
