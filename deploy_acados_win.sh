9a5c25959de0d2377c44400cfe7c1d4696d9d8d1
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-08-05 11:47:05 +0200

    output dir short id

--- deploy_acados_win.sh
+++ deploy_acados_win.sh
@@ -1,12 +1,13 @@
 ACADOS_COMMIT_ID=41d8e7df52f43ed20057a16df4c846be186c5fc6
-ACADOS_VERSION=41d8e7_win
+ACADOS_SHORT_ID=41d8e7
+ACADOS_VERSION=${ACADOS_SHORT_ID}_win
 OUTPUT_DIR=output_tmp_$ACADOS_VERSION
-
-DOWNLOAD_URL=https://github.com/acados/acados/archive/$ACADOS_COMMIT_ID.zip
-wget $DOWNLOAD_URL -O acados_src.zip
 
 mkdir $OUTPUT_DIR
 mkdir $OUTPUT_DIR/lib
+
+DOWNLOAD_URL=https://github.com/acados/acados/archive/$ACADOS_COMMIT_ID.zip
+wget $DOWNLOAD_URL -O acados_src.zip
 
 mv acados_src.zip $OUTPUT_DIR
 cp README.txt $OUTPUT_DIR
@@ -15,5 +16,5 @@ cp lib/libblasfeo.lib $OUTPUT_DIR/lib
 cp lib/libhpipm.lib $OUTPUT_DIR/lib
 
 cd $OUTPUT_DIR
-zip -r ../acados-${ACADOS_VERSION}.zip .
+zip -r -q acados-${ACADOS_VERSION}.zip .
 cd ..
