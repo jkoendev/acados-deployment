cefc528968f95b1bc97965e82d339e938d8dd01d
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-09-13 17:25:40 +0200

    Compile acados for win and linux in WSL, version update 53b661

--- deploy_acados.sh
+++ deploy_acados.sh
@@ -1,7 +1,7 @@
 set -e
 
-ACADOS_COMMIT_ID=7a83d3ee2c67d2453a3c873b832a94b0a18c6f50
-ACADOS_SHORT_ID=7a83d3
+ACADOS_COMMIT_ID=53b661f99e526d1bf5be166a9b552641df361219
+ACADOS_SHORT_ID=53b661
 
 git clone https://github.com/acados/acados.git
 cd acados
@@ -20,12 +20,19 @@ cd ..
 
 mkdir $OUTPUT_DIR/lib
 
+# compile for linux
+mkdir acados_build
+cd acados_build
+cmake -D BLASFEO_TARGET=GENERIC -D HPIPM_TARGET=GENERIC -D ACADOS_INSTALL_DIR=../acados_install -DBUILD_SHARED_LIBS=ON ../$OUTPUT_DIR
+make
+make install
+cd ..
+
 cp README.txt $OUTPUT_DIR
-cp lib_linux/libacore.so $OUTPUT_DIR/lib
-cp lib_linux/libacados.so $OUTPUT_DIR/lib
-cp lib_linux/libblasfeo.so $OUTPUT_DIR/lib
-cp lib_linux/libhpipm.so $OUTPUT_DIR/lib
-cp lib_linux/blasfeo_target.h $OUTPUT_DIR/external/blasfeo/include/
+cp acados_install/lib/libacados.so $OUTPUT_DIR/lib
+cp acados_install/lib/libblasfeo.so $OUTPUT_DIR/lib
+cp acados_install/lib/libhpipm.so $OUTPUT_DIR/lib
+cp acados_install/include/blasfeo/include/blasfeo_target.h $OUTPUT_DIR/external/blasfeo/include/
 
 cd $OUTPUT_DIR
 zip -r -q acados-${ACADOS_VERSION}.zip .
@@ -33,6 +40,8 @@ cd ..
 mv $OUTPUT_DIR/acados-${ACADOS_VERSION}.zip output/acados-${ACADOS_VERSION}.zip
 
 rm -rf $OUTPUT_DIR
+rm -rf acados_build
+rm -rf acados_install
 
 # windows
 ACADOS_VERSION=${ACADOS_SHORT_ID}_win
@@ -44,11 +53,19 @@ cd ..
 
 mkdir $OUTPUT_DIR/lib
 
+# compile for windows
+mkdir acados_build
+cd acados_build
+cmake.exe -G "MinGW Makefiles" -D BLASFEO_TARGET=GENERIC -D HPIPM_TARGET=GENERIC -D ACADOS_INSTALL_DIR=../acados_install -DBUILD_SHARED_LIBS=OFF ../$OUTPUT_DIR
+mingw32-make.exe
+mingw32-make.exe install
+cd ..
+
 cp README.txt $OUTPUT_DIR
-cp lib_win/libacados.lib $OUTPUT_DIR/lib
-cp lib_win/libblasfeo.lib $OUTPUT_DIR/lib
-cp lib_win/libhpipm.lib $OUTPUT_DIR/lib
-cp lib_win/blasfeo_target.h $OUTPUT_DIR/external/blasfeo/include/
+cp acados_install/lib/libacados.lib $OUTPUT_DIR/lib
+cp acados_install/lib/libblasfeo.lib $OUTPUT_DIR/lib
+cp acados_install/lib/libhpipm.lib $OUTPUT_DIR/lib
+cp acados_install/include/blasfeo/include/blasfeo_target.h $OUTPUT_DIR/external/blasfeo/include/
 
 cd $OUTPUT_DIR
 zip -r -q acados-${ACADOS_VERSION}.zip .
@@ -56,6 +73,8 @@ cd ..
 mv $OUTPUT_DIR/acados-${ACADOS_VERSION}.zip output/acados-${ACADOS_VERSION}.zip
 
 rm -rf $OUTPUT_DIR
+rm -rf acados_build
+rm -rf acados_install
 
 # cleanup
 rm -rf acados
